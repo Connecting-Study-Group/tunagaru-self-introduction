d99c79c2674701810cd16a69b079c6bb9d7f86bf
ユウト <codeyy021@gmail.com>
2023-01-07 21:03:32 +0900

    👔 entityとrepositoryを追加

---
+++ client/src/modules/user.entity.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  company: string;
+  job: string;
+  status: string;
+  bio: string;
+  createdAt: string;
+  updatedAt: string;
+};
