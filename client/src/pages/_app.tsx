c7ed3e8caeb5b88d3531fe251060a58b23ab7491
ユウト <codeyy021@gmail.com>
2023-01-07 14:04:11 +0900

    ✨ getLayoutの設定

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,30 +1,28 @@
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { SessionProvider } from 'next-auth/react';
+import { useEffect } from 'react';
+import type { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { AppPropsWithLayout } from '@/next-type';
+import { MantineProvider } from '@mantine/core';
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout): ReactNode {
+  useEffect(() => {
+    document.documentElement.style.visibility = 'visible';
+  }, []);
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
+  return getLayout(
+    <SessionProvider session={pageProps?.session}>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
+          fontFamily: '"Helvetica Neue",Arial,"Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif',
         }}
       >
         <Component {...pageProps} />
+        <Toaster />
       </MantineProvider>
-    </>
+    </SessionProvider>
   );
 }
