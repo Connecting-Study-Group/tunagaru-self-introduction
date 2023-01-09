5b50c37314393c6113f13609f01aaec3a25857da
ユウト <codeyy021@gmail.com>
2023-01-09 22:43:41 +0900

    🚀

--- client/scripts/deploy.sh
+++ client/scripts/deploy.sh
@@ -1,7 +1,21 @@
 set -eux
 
-# /../openapi/outputs/openapi_client/tsのプロジェクトをコピー
-"`dirname $0`"/setup_production_openapi.sh
+OPENAPI_PATH="../openapi/outputs/openapi_client/ts"
+PRODUCTION_OPENAPI_PATH="./openapi_client/ts"
+
+rm -rf $PRODUCTION_OPENAPI_PATH
+mkdir -p $PRODUCTION_OPENAPI_PATH
+# openapiのファイルをプロジェクト内にコピー
+cp -r $OPENAPI_PATH/* $PRODUCTION_OPENAPI_PATH
+
+# package.jsonで読み込んでるmio-openapi-server-interfaceのパスを書き換える
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e "s|$OPENAPI_PATH|$PRODUCTION_OPENAPI_PATH|g" package.json
+else
+  sed -i -e "s|$OPENAPI_PATH|$PRODUCTION_OPENAPI_PATH|g" package.json
+fi
+
+pnpm install
 
 # ビルド
 pnpm build
